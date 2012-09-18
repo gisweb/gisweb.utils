afb48854b9b52ad9ecc7d5aada6ba2e63420fde7
Manuele <manuele@inventati.org>
2012-09-18 15:49:00 +0200

    get_gridDataFor debug

--- src/gisweb/utils/plomino_utils.py
+++ src/gisweb/utils/plomino_utils.py
@@ -241,14 +241,15 @@ def get_dataFor(plominoDocument, where, items=None, render='as_list', filter_fun
     grid_name = None
     sub_form_name = None
     
-    db = plominoDocument.getParentDatabase()
-    sub_form = db.getForm(sub_form_name)
     if where in plominoDocument.getItems():
         grid_name = where
-        grid_field = sub_form.getFormField(grid_name)
+        db = plominoDocument.getParentDatabase()
+        form = db.getForm(form_name or plominoDocument.Form)
+        grid_field = form.getFormField(grid_name)
         sub_form_name = grid_field.getSettings(key='associated_form')
     else:
         sub_form_name = where
+    sub_form = db.getForm(sub_form_name)
 
     if not items:
         items = [f.id for f in sub_form.getFormFields(includesubforms=True)]
