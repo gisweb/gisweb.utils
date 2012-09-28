c36d8d7b08cb27f4b694865e1581ac8983cee7c4
Manuele <manuele@inventati.org>
2012-09-28 14:55:46 +0200

    function getAllSubforms debugged

--- src/gisweb/utils/plomino_utils.py
+++ src/gisweb/utils/plomino_utils.py
@@ -111,7 +111,7 @@ def getAllSubforms(frm, doc=None, applyhidewhen=False):
     Get sub forms recursively.
     """
     
-    db = frm.getParentDatabase()
+    #db = frm.getParentDatabase()
     
     sub_forms = []
 
