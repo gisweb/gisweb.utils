430dfe05f75a2d022d6bceee4f71989639a3cc77
Manuele <manuele@inventati.org>
2012-10-05 22:07:18 +0200

    added suggestFromTable to db_utils

--- src/gisweb/utils/db_utils.py
+++ src/gisweb/utils/db_utils.py
@@ -30,50 +30,41 @@ def get_session(sessionname):
 #    
 #    return 1
 
-def query_table(sessionname, name, schema='public', flt=None, test=True):
-    '''
-    table_infos = dict(name='<table_name>', schema='<schema_name>')
-    flt = {'<column_name>': {'query': <value>, 'operator': ..., }}
-    '''
-
-    session = get_session(sessionname)
-#    session = named_scoped_session(sessionname) 
-    engine = session.get_bind()
-    db = SqlSoup(engine)
-    
-    table = db.entity(name, schema=schema)
-    
-    if flt:
-        for k,v in flt.items():
-            table[k]
-    
-    if test:
-        return table.filter(flt).all()
-    else:
-        return table
-
 def suggestFromTable(sessionname, name, columnname, others=[], schema='public', tip='', **filters):
+    '''
+    utile per l'implementazione di semplici servizi di auto-suggest da tabella.
+    sessionname: neme della sessione
+    name: nome della tabella
+    columnname: nome della colonna
+    others: altre colonne cui si è interessati al valore. Usare '' per tutte.
+    schema: nome dello schema di appartenenza della tabella
+    tip: "suggerimento"
+    filters: filtri aggiuntivi del tipo <chiave>=<valore>
+    '''
+
     session = get_session(sessionname)
     engine = session.get_bind()
     db = SqlSoup(engine)
-    
     table = db.entity(name, schema=schema)
     
     if isinstance(others, (list, tuple)):
+        # you can submit a list of column values to return, at least an empty list
         query = session.query(*[table.c[col] for col in [columnname]+list(others)])
+    elif others == 'filters_only':
+        # easter egg: useful?
+        query = session.query(*[table.c[col] for col in [columnname]+filters.keys()])
     else:
         # otherwise all columns will be returned
         query = table
-    
+
     column = table.c[columnname]
+
+    # ilike '%(tip)s%%'
     where = or_(column.startswith(tip), column.startswith(tip.capitalize()), column.startswith(tip.lower()))
-    
+    # other simple filters
     where = and_(where, *[(table.c[k]==v) for k,v in filters.items() if k in table.c.keys()])
-    
-#    row2dict = lambda r: dict([(c.name, getattr(r, c.name)) for c in r.__table__.columns])
-    
+
     return [row.__dict__ for row in query.filter(where).all()]
-    
 
 def plominoSqlSync(session, plominoDocument, **table_infos):
     """
