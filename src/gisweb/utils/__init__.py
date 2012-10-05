05803639b57bc7a82939017a7e8ed48f5100cfc5
Manuele <manuele@inventati.org>
2012-10-05 15:11:14 +0200

    test db connection utils

--- src/gisweb/utils/__init__.py
+++ src/gisweb/utils/__init__.py
@@ -56,7 +56,7 @@ from pdf_utils import generate_pdf
 ##################################################################### DB UTILS #
 
 from db_utils import get_session, plominoSqlSync
-from db_utils import get_tab
+from db_utils import query_table
 
 
 ############################################################### CF P.IVA UTILS #
