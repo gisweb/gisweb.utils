77765f45501c6b8f1183cc75158aba57ed475171
Manuele <manuele@inventati.org>
2012-10-05 14:31:13 +0200

    test db connection utils

--- src/gisweb/utils/db_utils.py
+++ src/gisweb/utils/db_utils.py
@@ -35,7 +35,8 @@ def get_tab(session_id, table_name, table_infos):
     table_infos = dict(name='<table_name>', schema='<schema_name>')
     '''
 
-    session = get_session(session_id)
+#    session = get_session(session_id)
+    session = named_scoped_session(sessionname) 
     engine = session.get_bind()
     db = SqlSoup(engine, session=session)
     
