93d459a716c0b6850d35b077efb5aa4aaba7082d
Manuele Pesenti <manuele@mint>
2012-10-23 10:25:56 +0200

    added spezia_utils woth protocollo tools

---
+++ src/gisweb/utils/spezia_utils.py
@@ -0,0 +1,101 @@
+# - coding: utf-8 -
+
+from lxml import etree
+#from datetime import datetime
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
+
+def initBody4spezia():
+
+    page = etree.Element('xml', version='1.0', encoding='ISO-8859-1')
+    doc = etree.ElementTree(page)
+    
+    segnatura_info = {'{http://www.w3.org/XML/1998/namespace}lang': 'it'}
+    segnatura = etree.SubElement(page, 'Segnatura', **segnatura_info)
+    
+    intestazione = etree.SubElement(segnatura, 'Intestazione')
+
+    identificatore = etree.SubElement(intestazione, 'Identificatore')
+
+    parent = etree.SubElement(identificatore, 'CodiceAmministrazione')
+    id_aoo = etree.SubElement(identificatore, 'CodiceAOO')
+    idRichiesta = etree.SubElement(identificatore, 'NumeroRegistrazione')
+    data_segnatura = etree.SubElement(identificatore, 'DataRegistrazione')
+    
+    origine = etree.SubElement(intestazione, 'Origine')
+
+    indirizzotelematico = etree.SubElement(origine, 'IndirizzoTelematico')
+    mittente = etree.SubElement(origine, 'Mittente')
+
+    amministrazione = etree.SubElement(mittente, 'Amministrazione')
+    nominativo = etree.SubElement(amministrazione, 'Denominazione')
+    unitaorganizzativa = etree.SubElement(amministrazione, 'UnitaOrganizzativa')
+    denominazione = etree.SubElement(unitaorganizzativa, 'Denominazione')
+    indirizzopostale = etree.SubElement(unitaorganizzativa, 'IndirizzoPostale')
+    indirizzo = etree.SubElement(indirizzopostale, 'Toponimo')
+    civico = etree.SubElement(indirizzopostale, 'Civico')
+    cap = etree.SubElement(indirizzopostale, 'CAP')
+    comune = etree.SubElement(indirizzopostale, 'Comune')
+    provincia = etree.SubElement(indirizzopostale, 'Provincia')
+    
+    aoo = etree.SubElement(mittente, 'AOO')
+    denominazione = etree.SubElement(aoo, 'Denominazione')
+    
+    destinazione = etree.SubElement(intestazione, 'Destinazione', confermaRicezione='si')
+    IndirizzoTelematico = etree.SubElement(destinazione, 'IndirizzoTelematico', tipo='uri')
+    
+    risposta = etree.SubElement(intestazione, 'Risposta')
+    responseURL = etree.SubElement(risposta, 'IndirizzoTelematico')
+    
+    riservato = etree.SubElement(intestazione, 'Riservato')
+    riservato.text = 'N'
+    
+    oggetto = etree.SubElement(intestazione, 'Oggetto')
+    note = etree.SubElement(intestazione, 'Note')
+    
+    riferimenti = etree.SubElement(segnatura, 'Riferimenti')
+    
+    contestoprocedurale = etree.SubElement(riferimenti, 'ContestoProcedurale')
+    
+    utenteProtocollatore  = etree.SubElement(contestoprocedurale, 'CodiceAmministrazione')
+    cp_aoo = etree.SubElement(contestoprocedurale, 'CodiceAOO')
+    identificativo = etree.SubElement(contestoprocedurale, 'Identificativo')
+    tipocontesto = etree.SubElement(contestoprocedurale, 'TipoContestoProcedurale')
+    classifica = etree.SubElement(contestoprocedurale, 'Classifica')
+
+    titolario = etree.SubElement(classifica, 'Livello')
+    classifica = etree.SubElement(classifica, 'Livello')
+    cl_l3 = etree.SubElement(classifica, 'Livello')
+    cl_l3.text = '0'
+    
+    descrizione = etree.SubElement(segnatura, 'Descrizione')
+    documento = etree.SubElement(descrizione, 'Documento', tipoRiferimento='MIME')
+    
+    return locals()
+
+def getXmlBody(
+        parent = 86,
+        titolario = '6',
+        classifica = '7',
+        tipocontesto = 'IndicazioneClassificazione',
+        responseURL = "http://protocollo/ricevitore/ricevitore.php",
+        **kwdata):
+
+    data = locals()
+    kwdata = data.pop('kwdata')
+    data.update(kwdata)
+
+    bodyParts = initBody4spezia()
+
+    for k,v in data.items():
+        bodyParts[k].text = v
+    
+    xmlfile = StringIO()
+    bodyParts['doc'].write(xmlfile)
+    body = xmlfile.getvalue()
+    xmlfile.close()
+
+    return body
+
