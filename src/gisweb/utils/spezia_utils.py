b15f713ae61a065fa9ff10883031aa7e89cc7559
Manuele Pesenti <manuele.pesenti@gmail.com>
2012-10-31 15:38:22 +0100

    debug

--- src/gisweb/utils/spezia_utils.py
+++ src/gisweb/utils/spezia_utils.py
@@ -111,15 +111,28 @@ def getXmlBody(
 
     bodyParts = initBody4spezia()
 
+#    def valid_XML_char_ordinal(i):
+#        return ( # conditions ordered by presumed frequency
+#            0x20 <= i <= 0xD7FF 
+#            or i in (0x9, 0xA, 0xD)
+#            or 0xE000 <= i <= 0xFFFD
+#            or 0x10000 <= i <= 0x10FFFF
+#            )
+
     for k,v in data.items():
         if k in bodyParts:
             if isinstance(v, basestring):
-                flag = v
+                flag = u'%s' % v
             elif hasattr(v, 'strftime'):
-                flag = v.strftime('%Y-%m-%d %H:%M:%S')
+                flag = u'%s' % v.strftime('%Y-%m-%d %H:%M:%S')
             else:
-                flag = '%s' % v
+                flag = u'%s' % v
+                
             bodyParts[k].text = flag.encode('utf-8')
+            
+#            bodyParts[k].text = ''.join(
+#                c for c in flag if valid_XML_char_ordinal(ord(c))
+#                ).encode('utf-8')
     
     xmlfile = StringIO()
     bodyParts['doc'].write(xmlfile)
