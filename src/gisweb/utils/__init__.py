2983ef82e050f93300f96e1f6df45fc13897eecf
Manuele Pesenti <manuele.pesenti@gmail.com>
2012-11-06 13:32:59 +0100

    plominoKin debug. Removed get_parent_plominodb because it was a getParentDatabase method duplcation

--- src/gisweb/utils/__init__.py
+++ src/gisweb/utils/__init__.py
@@ -12,7 +12,6 @@ def initialize(con):
 
 ################################################################ PLOMINO UTILS #
 
-from plomino_utils import get_parent_plominodb
 from plomino_utils import attachThis
 from plomino_utils import ondelete_parent, oncreate_child, onsave_child, ondelete_child, create_child
 from plomino_utils import get_children_list, get_parent
