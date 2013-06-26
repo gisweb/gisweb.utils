4fd1de90af3685f89fe5e43c48f71d59237dbe1c
Manuele Pesenti <manuele.pesenti@gmail.com>
2013-06-26 14:36:24 +0200

    better json utils functions

--- src/gisweb/utils/json_utils.py
+++ src/gisweb/utils/json_utils.py
@@ -1,4 +1,6 @@
-import simplejson as json
+#import simplejson as json
+
+import json
 
 import locale
 
@@ -7,19 +9,36 @@ try:
 except:
     locale.setlocale(locale.LC_TIME, '')
 
-def handler(obj):
-    if hasattr(obj, 'strftime'):
-        if obj.strftime('%H:%M:%S') == '00:00:00':
-            srepr = '%d/%m/%Y'
-#            srepr = '%a %d %b %Y'
-        else:
-            srepr = '%d/%m/%Y %H:%M:%S'
-        return obj.strftime(srepr)
-    else:
-        return None
 
-def json_dumps(pyobj=''):
-    return json.dumps(pyobj, default=handler)
+def handler(obj, format='ISO'):
+
+    if hasattr(obj, 'strftime'):
+        if format == 'ISO':
+            return obj.ISO()
+        else:
+            return obj.strftime(format)
+    else:
+        return obj
+    
+#def handler(obj):
+    #if hasattr(obj, 'strftime'):
+        #if obj.strftime('%H:%M:%S') == '00:00:00':
+            #srepr = '%d/%m/%Y'
+##            srepr = '%a %d %b %Y'
+        #else:
+            #srepr = '%d/%m/%Y %H:%M:%S'
+        #return obj.strftime(srepr)
+    #else:
+        #return None
+
+def json_dumps(obj, dateformat='%d/%m/%Y %H:%M:%S', **kwargs):
+
+    kw = dict(
+        default=lambda o: handler(o, format=dateformat),
+        **kwargs
+    )
+    
+    return json.dumps(obj, **kw)
 
 def json_loads(string, **kwargs):
     return json.loads(string, **kwargs)
